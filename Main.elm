5a4d031f0ed319325bcfea0397d7d73848526f20
Ashish Negi <ashishnegi33@gmail.com>
2017-07-23 16:35:54 +0530

    First integration.. :)

--- Main.elm
+++ Main.elm
@@ -1,12 +1,35 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (Html, program)
 import Quadrant.Model as QM
 import Quadrant.Message as QMsg
 import Quadrant.Update as QUpd
 import Quadrant.View as QV
 
 main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+view : QM.QuadrantModel -> Html QMsg.Msg
+view model =
     Html.div []
         [ Html.text "Quadrant view of life."
-        , QV.view (QM.QuadrantModel  [] (QM.ViewData "" QM.UrgentAndImportant 200))]
+        , QV.view model
+        ]
+
+
+init : (QM.QuadrantModel, Cmd QMsg.Msg)
+init =
+    (QM.QuadrantModel [] (QM.ViewData "" QM.UrgentAndImportant 200), Cmd.none)
+
+
+update : QMsg.Msg -> QM.QuadrantModel -> ( QM.QuadrantModel, Cmd QMsg.Msg )
+update =
+    QUpd.update
+
+subscriptions x = Sub.none
