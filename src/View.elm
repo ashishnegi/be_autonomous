b2594ef19606716ee2ecf6b7b1eb81bb16627f1f
Ashish Negi <ashishnegi33@gmail.com>
2017-08-12 18:30:38 +0530

    Added footer

--- src/View.elm
+++ src/View.elm
@@ -8,6 +8,8 @@ import Material.Scheme
 import Material.Color as Color
 import Material.Grid as Grid
 import Material.Layout as Layout
+import Material.Footer as Footer
+import Material.Options as Options
 
 
 view : Model -> Html Msg.Msg
@@ -23,16 +25,43 @@ view model =
             , drawer = []
             , tabs = ( [], [] )
             , main =
-                [ Grid.grid
-                    []
-                    [ Grid.cell [ Grid.size Grid.All 2 ]
-                        -- Todo: remove in mobile
-                        []
-                    , Grid.cell [ Grid.size Grid.All 10 ]
-                        [ Html.map Msg.Quadrant (QV.view model.quadrant)
-                        ]
-                    , Grid.cell [ Grid.size Grid.All 2 ]
-                        []
+                [ Grid.grid []
+                    [ Grid.cell [ Grid.size Grid.All 12 ]
+                        [ quadrantView model ]
+                    , Grid.cell [ Grid.size Grid.All 12 ]
+                        [ footerView ]
                     ]
                 ]
             }
+
+
+quadrantView : Model -> Html Msg.Msg
+quadrantView model =
+    Grid.grid
+        []
+        [ Grid.cell [ Grid.size Grid.All 2 ]
+            -- Todo: remove in mobile
+            []
+        , Grid.cell [ Grid.size Grid.All 10 ]
+            [ Html.map Msg.Quadrant (QV.view model.quadrant)
+            ]
+        , Grid.cell [ Grid.size Grid.All 2 ]
+            []
+        ]
+
+
+footerView : Html Msg.Msg
+footerView =
+    Footer.mini []
+        { left =
+            Footer.left []
+                [ Footer.logo [] [ Footer.html <| text "Track your life..." ]
+                ]
+        , right =
+            Footer.right []
+                [ Footer.logo [] [ Footer.html <| text "Like us @ " ]
+                , Footer.socialButton [ Options.css "margin-right" "6px" ] []
+                , Footer.socialButton [ Options.css "margin-right" "6px" ] []
+                , Footer.socialButton [ Options.css "margin-right" "0px" ] []
+                ]
+        }
