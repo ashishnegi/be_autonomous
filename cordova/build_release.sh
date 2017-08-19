a0109f76aa7621fa124f6e2c66919dd19f333db1
Ashish Negi <ashishnegi33@gmail.com>
2017-08-19 13:42:15 +0530

    Version 2 cordova app

--- cordova/build_release.sh
+++ cordova/build_release.sh
@@ -2,5 +2,5 @@
 rm *apk
 cordova build android --release --keystore=android_playstore.keystore --storePassword=android_playstore --alias=android_playstore
 cp /home/ashish/work/be_autonomous/cordova/platforms/android/build/outputs/apk/android-release-unsigned.apk BeAutonomousReleaseSigned.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android_playstore.keystore BeAutonomousReleaseSigned.apk android_playstore
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../android_playstore.keystore BeAutonomousReleaseSigned.apk android_playstore
 /home/ashish/android/build-tools/26.0.1/zipalign -v 4 BeAutonomousReleaseSigned.apk BeAutonomous.apk
