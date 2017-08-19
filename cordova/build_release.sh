03704a9e6cfc6caa372ea4c7ffb53d4314bed9d6
Ashish Negi <ashishnegi33@gmail.com>
2017-08-19 15:49:58 +0530

    Ignore : Remove spec and res files : version 0.0.3

--- cordova/build_release.sh
+++ cordova/build_release.sh
@@ -1,6 +1,6 @@
 # increase version number in config.xml
 rm *apk
-cordova build android --release --keystore=android_playstore.keystore --storePassword=android_playstore --alias=android_playstore
+cordova build android --release --keystore=android_playstore.keystore --storePassword=android_playstore --alias=android_playstore --verbose
 cp /home/ashish/work/be_autonomous/cordova/platforms/android/build/outputs/apk/android-release-unsigned.apk BeAutonomousReleaseSigned.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../android_playstore.keystore BeAutonomousReleaseSigned.apk android_playstore
 /home/ashish/android/build-tools/26.0.1/zipalign -v 4 BeAutonomousReleaseSigned.apk BeAutonomous.apk
